70091e233a17defc9430f6908cf5ea61fdf20d8a
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-12 21:40:35 -0300

    add very basics

---
+++ 0-very-basics.js
@@ -0,0 +1,9 @@
+/*
+    Very basics
+    - This is a multi-line comment
+*/
+
+// This is a single line comment
+
+// Output text to the console. "console" is a built-in object; "log" is the method
+console.log("Print this sentence.")
