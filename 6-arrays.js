ca7205e7838a9055f280968014200727fa5d84c8
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-20 13:08:29 -0300

    add unshift/shift, use of const, nested arrays, and refactor comments

--- 6-arrays.js
+++ 6-arrays.js
@@ -1,23 +1,56 @@
 /*
     Arrays
-    - 0-indexed
-    - accept mutiple data types
+    - Are 0-indexed
+    - Are mutable
+    - Can accept mixed data types
 */
 
+// [CREATE]
 // Create an array
 let arrStrings = ["A", "B", "C"];
 let arrIntegers = [1, 2, 3, 4, 14];
 let arrBooleans = [true, true, false];
 let arrMixed = ["A", 2, "z", true];
 
+// About using "const" with array
+const arrConst = [1, 2, 3];
+// - This is valid. Can still modify, add, remove elements
+arrConst[1] = 0;                    // [1, 0, 3]
+// - This is NOT ok. Can't be reassigned
+// arrConst = [1, 2];               // TypeError: Assignment to constant variable
+
+
+
+// [ACCESS AND UPDATE]
 // Access elements by index
 arrStrings[2];                      // "C"
 
 // Update elements by index
 arrMixed[3] = "B";                  // arrMixed: ['A', 2, 'z', 'B']
-                                    
+
+
+
+// [PROPERTIES]
+// Length
+arrStrings.length;                  // 3
+
+
+
+// [METHODS]
+// Add element to the start and shift indexes up one place
+arrMixed.unshift("1A");             // arrMixed: ['1A', 'A', 2, 'z', 'B']
+
 // Add element to the end
-arrMixed.push("XX");                // arrMixed: ['A', 2, 'z', 'B', 'XX']
+arrMixed.push("Z");                 // arrMixed: ['1A', 'A', 2, 'z', 'B', 'Z']
+
+// Remove first element and shift indexes down one place
+arrMixed.shift();                   // arrMixed: ['A', 2, 'z', 'B', 'Z']
 
 // Remove last element (returns element removed)
 arrMixed.pop();                     // arrMixed: ['A', 2, 'z', 'B']
+
+
+
+// [NESTED ARRAYS]
+arrNested = [[1, 2, 3], [4, 5], [6], [7, 8, 9, 10, 11]]
+arrNested[3][3];                    // 10
