1b8d835b7de8a8916d4fd76e7ab59d1205cdb1a0
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-26 13:15:39 -0300

    add high order functions, high-class concept, and fix name in 005-functions-2.js

--- 005-functions-2.js
+++ 005-functions-2.js
@@ -1,6 +1,7 @@
 /*
-    Functions: Part B
-    - Functions are a type of object; they have properties and methods
+    Functions: Part 2
+    - Functions are "first-class" objects, meaning 
+      they have properties and methods.
 */
 
 function discipline() {
@@ -8,10 +9,11 @@ function discipline() {
 }
 
 // Variable "gooo" holds a reference to the function
-const gooo = discipline;
+const gooo = discipline;   // No ()!
 
-// Invoke the function using the reference
+// Invoke the function using the reference to the function
 gooo();                         // 'Keep the f*ck going!'
+
 
 
 // [PROPERTIES]
@@ -22,6 +24,20 @@ console.log(gooo.name);         // 'discipline'
 console.log(gooo.length);       // 0
 
 
+
 // [METHODS]
 // - .toString(): returns the source of the object as a string
 console.log(gooo.toString());
+
+
+
+// [HIGH ORDER FUNCTIONS]
+const sum = (n1, n2) => {
+    return n1 + n2;
+}
+
+const analyzeRequest = (func, val1, val2) => {
+    return func(val1, val2);
+}
+
+console.log( analyzeRequest(sum, 5, 6) )    // 11
