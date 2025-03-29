2ac973c8c5eb0624d1eb5721714a5dffcabcbc0c
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-29 17:38:09 -0300

    add empty object, nested object and comments in 011-objects.js

--- 011-objects.js
+++ 011-objects.js
@@ -1,13 +1,21 @@
 /*
     Objects
+    - Contain unordered data, organized in key-value pairs.
 */
 
 // [CREATE]
+// - Empty object
+let car = {}
+
 // - Keys: name, year. Values: SpaceExploration Inc., year
 // - Methods: printCEO
 let company = {    
     name: 'SpaceExploration Inc.',
     year: 2025,
+    contact: {                              // Nested object
+        email: 'ghostaddress@notf0und.c000m',
+        address: 'Void 0, Mars'
+    },
     printCEO () {
         console.log('Martin is the CEO');
     }
@@ -15,16 +23,21 @@ let company = {
 
 
 
-// [ACCESS]
+// [ACCESS PROPERTIES]
 // - Using dot operator
-console.log(company.year);              // 2025
+console.log(company.year);                  // 2025
 
 // - Using bracket notation
-console.log(company['year']);           // 2025
+console.log(company['year']);               // 2025
 
 // - Bracket notation also allows passing variables
 let y = 'year';
-console.log(company[y]);                // 2025
+console.log(company[y]);                    // 2025
+
+// - Accessing properties of nested object
+console.log(company.contact.address);       // Void 0, Mars
+console.log(company["contact"].address);    // Void 0, Mars
+console.log(company["contact"]["address"]); // Void 0, Mars
 
 
 
